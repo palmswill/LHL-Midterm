99f64012fce794c4a9ebe083f0e7c98c18c5046c
William Liu <liuwilly4@gmail.com>
2022-02-13 13:10:08 -0500

    minor css adjustment. added jquery footnote

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,12 +5,14 @@
 $(() => {
   // test data
 
+  // get request -get shop items
   const shopItems = renderShopItems(shopList);
 
   $(".shop").append(shopItems);
 
   console.log(" shop rendered");
 
+  // get request - get cart items
   const cartItem =renderCartItems(cartList);
 
   $(".basket").append(cartItem);
