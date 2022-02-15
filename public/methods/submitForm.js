c32ceb1e00e6e91ea525e1fc29a720b8b9f775fb
William Liu <liuwilly4@gmail.com>
2022-02-15 13:29:11 -0500

    adding order status page

--- public/methods/submitForm.js
+++ public/methods/submitForm.js
@@ -4,11 +4,11 @@ import { submitForm } from "./methods.js";
 export const initalizeForm = () => {
   // open form on check out
   $(".check-out").click(function () {
-    $(".pop-up").addClass("active");
+    $(".submitForm").addClass("active");
   });
   // close form on form submit/close form bottom;
   $(".cancel-modal,.submit-order").click(function () {
-    $(".pop-up").removeClass("active");
+    $(".submitForm").removeClass("active");
   });
 
 
